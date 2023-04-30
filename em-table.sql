85f319ea1fa2e4541ddb8d96c6c7f9b1236100f4
sridevipanjagala <131574963+sridevipanjagala@users.noreply.github.com>
2023-04-30 19:19:20 +0530

    Update em-table.sql
    inserted values

--- em-table.sql
+++ em-table.sql
@@ -1,3 +1,5 @@
 create database employee;
 use database;
 create table employee_details(employeeid int primary key auto_increment,employeename varchar(200) not null,phonenumber varchar(10) not null,employeecode char(10)  unique not null);
+insert into employee_details(employeename,phonenumber,employeecode)
+values ('sridevi','6300793005','odin001');
